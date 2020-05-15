b13ea9bca0d05b514bfc99583d28ec7d6581cd78
Neal Beeken <neal.beeken@mongodb.com>
2020-05-15 10:57:26 -0400

    feat: add cards

--- src/game/card.ts
+++ src/game/card.ts
@@ -1,16 +1,28 @@
 import { css, customElement, html, LitElement, property } from 'lit-element'
+import { DECK, HanaCard, Month } from './hanafuda'
 
 @customElement('hana-card')
 export class CardView extends LitElement {
-	@property() name = 'Spring'
+	@property()
+	get name() {
+		return this.card.name
+	}
+
+	@property({ type: HanaCard })
+	public card: HanaCard
+
+	constructor() {
+		super()
+		this.card = DECK.get(Month.Jan)[0]
+	}
 
 	static styles = css`
 		div {
-			outline: red;
+			outline: red solid 1px;
 		}
 	`
 
 	render() {
-		return html`<div>Hanafuda ${this.name} Card!</div> `
+		return html`<div class="tile">Hanafuda ${this.name} Card!</div>`
 	}
 }
---
+++ src/game/hanafuda.ts
@@ -0,0 +1,143 @@
+export enum Month {
+	Jan,
+	Feb,
+	Mar,
+	Apr,
+	May,
+	Jun,
+	Jul,
+	Aug,
+	Sep,
+	Oct,
+	Nov,
+	Dec,
+}
+
+export const enum Points {
+	Twenty = 20,
+	Ten = 10,
+	Five = 5,
+	One = 1,
+}
+
+interface HanaCardAttributes {
+	isScroll?: boolean
+	isAnimal?: boolean
+	hasPoetry?: boolean
+	isBasic?: boolean
+}
+
+export class HanaCard {
+	attributes: { isScroll: boolean; isAnimal: boolean }
+	constructor(
+		public month: Month,
+		public name: string,
+		public value: Points,
+		// attributes
+		attributes: HanaCardAttributes
+	) {
+		const defaultAttributes = { isScroll: false, isAnimal: false, isBasic: false, isWrittenOn: false }
+		this.attributes = { ...defaultAttributes, ...attributes }
+	}
+}
+
+type Suit = [HanaCard, HanaCard, HanaCard, HanaCard]
+
+const JanSuit: Suit = [
+	new HanaCard(Month.Jan, 'crane', Points.Twenty, { isAnimal: true }),
+	new HanaCard(Month.Jan, 'red_scroll_writing', Points.Five, { isScroll: true, hasPoetry: true }),
+	new HanaCard(Month.Jan, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Jan, 'basic', Points.One, { isBasic: true }),
+]
+
+const FebSuit: Suit = [
+	new HanaCard(Month.Feb, 'red_scroll_writing', Points.Five, { isScroll: true, hasPoetry: true }),
+	new HanaCard(Month.Feb, 'bush_warbler', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Feb, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Feb, 'basic', Points.One, { isBasic: true }),
+]
+
+const MarSuit: Suit = [
+	new HanaCard(Month.Mar, 'curtain', Points.Twenty, { isAnimal: true }),
+	new HanaCard(Month.Mar, 'red_scroll_writing', Points.Five, { isScroll: true, hasPoetry: true }),
+	new HanaCard(Month.Mar, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Mar, 'basic', Points.One, { isBasic: true }),
+]
+
+const AprSuit: Suit = [
+	new HanaCard(Month.Apr, 'red_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Apr, 'cuckoo', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Apr, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Apr, 'basic', Points.One, { isBasic: true }),
+]
+
+const MaySuit: Suit = [
+	new HanaCard(Month.May, 'red_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.May, 'docks', Points.Ten, { isBasic: false }),
+	new HanaCard(Month.May, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.May, 'basic', Points.One, { isBasic: true }),
+]
+
+const JunSuit: Suit = [
+	new HanaCard(Month.Jun, 'purple_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Jun, 'butterflies', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Jun, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Jun, 'basic', Points.One, { isBasic: true }),
+]
+
+const JulSuit: Suit = [
+	new HanaCard(Month.Jul, 'red_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Jul, 'boar', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Jul, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Jul, 'basic', Points.One, { isBasic: true }),
+]
+
+const AugSuit: Suit = [
+	new HanaCard(Month.Aug, 'moon', Points.Twenty, { isBasic: false }),
+	new HanaCard(Month.Aug, 'geese', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Aug, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Aug, 'basic', Points.One, { isBasic: true }),
+]
+
+const SepSuit: Suit = [
+	new HanaCard(Month.Sep, 'purple_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Sep, 'sake', Points.Ten, { isBasic: false, hasPoetry: true }),
+	new HanaCard(Month.Sep, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Sep, 'basic', Points.One, { isBasic: true }),
+]
+
+const OctSuit: Suit = [
+	new HanaCard(Month.Oct, 'purple_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Oct, 'doe', Points.Ten, { isAnimal: true }),
+	new HanaCard(Month.Oct, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Oct, 'basic', Points.One, { isBasic: true }),
+]
+
+const NovSuit: Suit = [
+	new HanaCard(Month.Nov, 'red_scroll', Points.Five, { isScroll: true }),
+	new HanaCard(Month.Nov, 'umbrella', Points.Twenty, { isAnimal: true }),
+	new HanaCard(Month.Nov, 'swallow', Points.Ten, { isBasic: false }),
+	new HanaCard(Month.Nov, 'lightning', Points.One, { isBasic: false }),
+]
+
+const DecSuit: Suit = [
+	new HanaCard(Month.Nov, 'phoenix', Points.Twenty, { isAnimal: true }),
+	new HanaCard(Month.Nov, 'basic', Points.Ten, { isBasic: true }),
+	new HanaCard(Month.Nov, 'basic', Points.One, { isBasic: true }),
+	new HanaCard(Month.Nov, 'basic', Points.One, { isBasic: true }),
+]
+
+export const DECK = new Map([
+	[Month.Jan, JanSuit],
+	[Month.Feb, FebSuit],
+	[Month.Mar, MarSuit],
+	[Month.Apr, AprSuit],
+	[Month.May, MaySuit],
+	[Month.Jun, JunSuit],
+	[Month.Jul, JulSuit],
+	[Month.Aug, AugSuit],
+	[Month.Sep, SepSuit],
+	[Month.Oct, OctSuit],
+	[Month.Nov, NovSuit],
+	[Month.Dec, DecSuit],
+])
